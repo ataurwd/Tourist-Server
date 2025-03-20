1a647a0953a631e2dd5623a2a2b74a43ca1e7111
ataurwd <ataurrahman24707@gmail.com>
2025-03-21 01:14:41 +0600

    update

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cors({
   credentials: true,
 }))
 
+// update code
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4jm04.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
